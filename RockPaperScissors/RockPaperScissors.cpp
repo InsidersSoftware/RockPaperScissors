caaf9a33e8723d94e3b7664875e99a5b0263dd80
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2020-03-07 19:53:18 -0800

    fixed some unwanted and ugly code
    Thanks FITCH

--- RockPaperScissors/RockPaperScissors.cpp
+++ RockPaperScissors/RockPaperScissors.cpp
@@ -1,46 +1,10 @@
 // RockPaperScissors.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include <vector>
 #include <iomanip>
-#include <stdlib.h>
 
 using namespace std;
 
-
-
-string randomName(int length) {
-
-    char consonents[] = { 'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z' };
-    char vowels[] = { 'a','e','i','o','u','y' };
-
-    string botName = "";
-
-    int random = rand() % 2;
-    int count = 0;
-
-    for (int i = 0; i < length; i++) {
-
-        if (random < 2 && count < 2) {
-            botName = botName + consonents[rand() % 19];
-            count++;
-        }
-        else {
-            botName = botName + vowels[rand() % 5];
-            count = 0;
-        }
-
-        random = rand() % 2;
-
-    }
-
-    return botName;
-
-}
-
-
-
+string randomName(int);
 
 int main()
 {
@@ -71,154 +35,139 @@ int main()
         cout << "Welcome " << playerTwo << endl;
         cout << "The Battle Between " << playerOne << " And " << playerTwo << endl;
         cout << "\nGLHF!!!\n";
-        cout << "Player One What Do You Choose: \n";
-        cout << "1.Rock\n";
-        cout << "2.Paper\n";
-        cout << "3.Scissors\n";
-        cin >> playerOneChoice;
-        //Logic
-        if (playerOneChoice == 1) {
+        cin.clear();
+        cout << playerOneChoice << endl;
+        while(playerOneChoice < 1 || playerOneChoice > 3){
+            cout << "Player One What Do You Choose: \n";
+            cout << "1.Rock\n";
+            cout << "2.Paper\n";
+            cout << "3.Scissors\n";
+            cin >> playerOneChoice;
+        }
+        
+        //Logic -cmi
+        cin.clear();
+        while(playerTwoChoice < 1 || playerTwoChoice > 3){
             cout << "Player Two What Do You Choose: \n";
             cout << "1.Rock\n";
             cout << "2.Paper\n";
             cout << "3.Scissors\n";
             cin >> playerTwoChoice;
-            if (playerTwoChoice == 1) {
-                cout << "TIE!!!\n";
-                cout << "Enter Feedback: ";
-                cin >> feedback;
-                return 0;
-            }
-            if (playerTwoChoice == 2) {
-                cout << "Player Two Win!!!\n";
-                cout << "Enter Feedback: ";
-                cin >> feedback;
-                return 0;
-            }
-            if (playerTwoChoice == 3) {
-                cout << "Player One Win!!!\n";
-                cout << "Enter Feedback: ";
-                cin >> feedback;
-                return 0;
-            }
         }
-        if (playerOneChoice == 2) {
-            cout << "Player Two What Do You Choose: \n";
-            cout << "1.Rock\n";
-            cout << "2.Paper\n";
-            cout << "3.Scissors\n";
-            cin >> playerTwoChoice;
-            if (playerTwoChoice == 1) {
-                cout << "Player One Win\n";
-                cout << "Enter Feedback: ";
-                cin >> feedback;
-                return 0;
-            }
-            if (playerTwoChoice == 2) {
-                cout << "TIE!!!\n";
-                cout << "Enter Feedback: ";
-                cin >> feedback;
-                return 0;
-            }
-            if (playerTwoChoice == 3) {
-                cout << "Player Two Win!!!\n";
-                cout << "Enter Feedback: ";
-                cin >> feedback;
-                return 0;
-            }
+
+        if (playerOneChoice == 1 && playerTwoChoice == 1) {
+            cout << "TIE!!!\n";
         }
-        if (playerOneChoice == 3) {
-            cout << "Player Two What Do You Choose: \n";
-            cout << "1.Rock\n";
-            cout << "2.Paper\n";
-            cout << "3.Scissors\n";
-            cin >> playerTwoChoice;
-            if (playerTwoChoice == 1) {
-                cout << "Player Two Win\n";
-                cout << "Enter Feedback: ";
-                cin >> feedback;
-                return 0;
-            }
-            if (playerTwoChoice == 2) {
-                cout << "Player One Win!!!\n";
-                cout << "Enter Feedback: ";
-                cin >> feedback;
-                return 0;
-            }
-            if (playerTwoChoice == 3) {
-                cout << "TIE!!!\n";
-                cout << "Enter Feedback: ";
-                cin >> feedback;
-                return 0;
-            }
+        if (playerOneChoice == 1 && playerTwoChoice == 2) {
+            cout << "Bot Win!!!\n";
         }
+        if (playerOneChoice == 1 && playerTwoChoice == 3) {
+            cout << "Player One Win!!!\n";
+        }
+        if (playerOneChoice == 2 && playerTwoChoice == 1) {
+            cout << "Player One Win!!!\n";
+        }
+        if (playerOneChoice == 2 && playerTwoChoice == 2) {
+            cout << "TIE!!!\n";
+        }
+        if (playerOneChoice == 2 && playerTwoChoice == 3) {
+            cout << "Bot Win!!!\n";
+        }
+        if (playerOneChoice == 3 && playerTwoChoice == 1) {
+            cout << "Bot Win!!!\n";
+        }
+        if (playerOneChoice == 3 && playerTwoChoice == 2) {
+            cout << "Player One Win!!!\n";
+        }
+        if (playerOneChoice == 3 && playerTwoChoice == 3) {
+            cout << "TIE!!!\n";
+        }
+
+        cin.clear();
+        cin.ignore();
+        cout << "Enter Feedback: ";
+        getline(cin, feedback);
+
     }
     if (gameMode == 2) {
         cout << "Enter Your Name First Player: ";
         cin >> playerOne;
         cout << "Welcome " << playerOne << endl;
-        cout << "Enter The Bot name length: " << endl;
-        int length;                                                   
+        cout << "Enter The Bot name length: ";
+        int length = 0;                                                   
         cin >> length;
-        cout << randomName(length) << endl;
-        cout << "Welcome " << " Bot " << randomName(length) << endl;
-        cout << "The Battle Between " << playerOne << " And " << " Bot " << randomName(length) << endl;
+        cout << endl;
+        //cout << randomName(length) << endl;
+        string randomName1 = randomName(length);
+        cout << "Welcome " << " Bot " << randomName1 << endl;
+        cout << "The Battle Between " << playerOne << " And " << "Bot " << randomName1 << endl;
         cout << "\nGLHF!!!\n";
         cout << "Player One What Do You Choose: \n";
         cout << "1.Rock\n";
         cout << "2.Paper\n";
         cout << "3.Scissors\n";
         cin >> playerOneChoice;
+
+        while(botChoice == 0)
+        {
+            botChoice = rand() % 3;
+        }
+
+        bool isTie = true;
+
         if (playerOneChoice == 1 && botChoice == 1) {
             cout << "TIE!!!\n";
-            return 0;
         }
         if (playerOneChoice == 1 && botChoice == 2) {
             cout << "Bot Win!!!\n";
-            return 0;
         }
         if (playerOneChoice == 1 && botChoice == 3) {
             cout << "Player One Win!!!\n";
-            cout << "Enter Feedback: ";
-            cin >> feedback;
-            return 0;
         }
         if (playerOneChoice == 2 && botChoice == 1) {
             cout << "Player One Win!!!\n";
-            cout << "Enter Feedback: ";
-            cin >> feedback;
-            return 0;
         }
         if (playerOneChoice == 2 && botChoice == 2) {
             cout << "TIE!!!\n";
-            cout << "Enter Feedback: ";
-            cin >> feedback;
-            return 0;
         }
         if (playerOneChoice == 2 && botChoice == 3) {
             cout << "Bot Win!!!\n";
-            cout << "Enter Feedback: ";
-            cin >> feedback;
-            return 0;
         }
         if (playerOneChoice == 3 && botChoice == 1) {
             cout << "Bot Win!!!\n";
-            cout << "Enter Feedback: ";
-            cin >> feedback;
-            return 0;
         }
         if (playerOneChoice == 3 && botChoice == 2) {
             cout << "Player One Win!!!\n";
-            cout << "Enter Feedback: ";
-            cin >> feedback;
-            return 0;
         }
         if (playerOneChoice == 3 && botChoice == 3) {
             cout << "TIE!!!\n";
-            cout << "Enter Feedback: ";
-                cin >> feedback;
-            return 0;
         }
+        cin.clear();
+        cin.ignore();
+        cout << "Enter Feedback: ";
+        getline(cin, feedback);
     }
 }
 
+string randomName(int length) {
+    char consonents[] = { 'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z' };
+    char vowels[] = { 'a','e','i','o','u','y' };
+    
+    string botName = "";
+
+    int random = rand() % 2;
+    int count = 0;
+
+    for (int i = 0; i < length; i++) {
+        if (random < 2 && count < 2) {
+            botName = botName + consonents[rand() % 19];
+            count++;
+        }
+        else {
+            botName = botName + vowels[rand() % 5];
+            count = 0;
+        }
+        random = rand() % 2;
+    }
+    return botName;
+}
